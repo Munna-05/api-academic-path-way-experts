05bd2d98d79f7a67d9af5429c6adaef189f8feca
Munna <sajeevmunna05@gmail.com>
2023-12-05 01:16:29 +0530

    all schemas created

--- Models/User.js
+++ Models/User.js
@@ -1,30 +1,37 @@
 import mongoose from "mongoose";
 
-const User = mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const User = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    phone:{
-        type:String,
-        required:true
+    phone: {
+      type: String,
+      required: true,
     },
-    address:{
-        type:String,
-        required:true
+    address: {
+      type: String,
+      required: true,
     },
-    pincode:{
-        type:String,
-        required:true
+    pincode: {
+      type: String,
+      required: true,
     },
-},{timestamps:true})
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model('User',User)
+export default mongoose.model("User", User);
