3be0ccddc0849fd06c9a731c4e076c985ab948e7
Munna <sajeevmunna05@gmail.com>
2023-12-05 01:21:59 +0530

    description pkg lok

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 const app = express()
 import userRoute from './Routes/UserRouter.js'
-import adminController from './Routes/AdminRouter.js'
+import adminRouter from './Routes/AdminRouter.js'
 import { handleError } from './Helpers/Error.js';
 
 app.use(cors())
@@ -15,16 +15,16 @@ let port = 5000 || 5001 || 5002
 const server = app.listen(port, () => {
 
     console.log(`server started ${port}`)
-    mongoose.connect('mongodb+srv://APE:APE_2023@cluster0.r8qezcf.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    mongoose.connect(process.env.MONGO).then(() => {
         console.log("DataBase connection successfull")
     }).catch(() => {
         console.log('Connection Error')
     })
-})
+}) 
 app.use((req,res,next)=>{
     console.log(req.url)
     next()
 })
-app.use('/admin', adminController)
+app.use('/admin', adminRouter)
 app.use('/', userRoute)    
 app.use(handleError)
